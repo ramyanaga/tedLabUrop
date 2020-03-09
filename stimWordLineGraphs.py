f09103ddc58bc840190b54b312d6bb36b1b9ebb7
Ramya Nagarajan <ramyan@mit.edu>
2020-03-09 00:14:30 -0400

    line graphs with averaged score per point

---
+++ stimWordLineGraphs.py
@@ -0,0 +1,79 @@
+import os, sys, inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+from representations.sequentialembedding import SequentialEmbedding
+
+import matplotlib as mpl
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+
+'''
+This function plots 3 lines per stim word --
+one for (stim, high), (stim, low), (stim, incon);
+each point of each line is the average of the similarity
+scores for years at and before that point
+'''
+
+def parseTxtFile():
+    """
+    This function dumps all (stim word, high, low, inconcgruent) into
+    one list per stim word
+    """
+    wordPairsList = []
+    with open("wordPairs.txt", "r") as f:
+        currentStimWordList = []
+        for line in f:
+            if " " in line:
+                wordPairsList.append(currentStimWordList)
+                currentStimWordList = []
+                splitLine = line.split(" ")
+                stimWord = splitLine[-1][:-2]
+                currentStimWordList.append(stimWord)
+            else:
+                currentStimWordList.append(line[:-2])
+
+    return wordPairsList[1:]
+
+
+def plotAveragedLines():
+    wordPairsList = parseTxtFile()
+    #print(len(wordPairsList))
+    figureCount = 0
+    years = [i for i in range(1900, 1990, 10)]
+    real_embeddings = SequentialEmbedding.load("../embeddings/eng-all_sgns", range(1900, 1990, 10))
+    yearHighVals, yearLowVals, yearInconVals = [], [], []
+    for wordList in wordPairsList:
+        if len(wordList) < 4:
+            continue
+        stimWord = wordList[0]
+        highAvgs, lowAvgs, inconAvgs = [], [], []
+        highCounter, lowCounter, inconCounter = 0,0,0
+        high, low, incongruent = wordList[1], wordList[2], wordList[3]
+        time_sim_high = real_embeddings.get_time_sims(stimWord, high)
+        time_sim_low = real_embeddings.get_time_sims(stimWord, low)
+        time_sim_incon = real_embeddings.get_time_sims(stimWord, incongruent)
+
+        for sim_year, sim in time_sim_high.iteritems():
+            nextHigh = (sum(highAvgs) + sim)/(len(highAvgs) + 1)
+            highAvgs.append(nextHigh)
+        
+        for sim_year, sim in time_sim_low.iteritems():
+            nextLow = (sum(lowAvgs) + sim)/(len(lowAvgs) + 1)
+            lowAvgs.append(nextLow)
+        
+        for sim_year, sim in time_sim_incon.iteritems():
+            nextIncon = (sum(inconAvgs) + sim)/(len(inconAvgs) + 1)
+            inconAvgs.append(nextIncon)
+
+        plt.figure()
+        plt.plot(years, highAvgs, 'g')
+        plt.plot(years, lowAvgs, 'r')
+        plt.plot(years, inconAvgs, 'm')
+        plotTitle = stimWord + " " + "(" + high + ", " + low + ", " + incongruent + ", " + ")"
+        plotFileName = "averagedLineGraphs/" + stimWord + "_" + high + "_" + low + "_" + incongruent
+        plt.title(plotTitle)
+        plt.savefig(plotFileName)
+        plt.close()
+
+plotAveragedLines()
